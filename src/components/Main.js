eb0df27b954142e88548825e9a254371dcff3e3f
kulinski <tsvetelin_kulinski@abv.bg>
2017-05-28 19:02:26 +0300

    Changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactList from 'react-list';
+import Scroll from 'react-scroll';
 import './Main.css';
 
-var Scroll    = require('react-scroll');
-
-var Element    = Scroll.Element;
-var Events     = Scroll.Events;
-var scroll     = Scroll.animateScroll;
-var scrollSpy  = Scroll.scrollSpy;
+var Element = Scroll.Element;
+var Events = Scroll.Events;
+var scroll = Scroll.animateScroll;
+var scrollSpy = Scroll.scrollSpy;
 
 class Main extends Component {
 
-  constructor (props){
-      super(props);
-      this.scrollToTop = this.scrollToTop.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      estates: []
+    };
+
+    this.scrollToTop = this.scrollToTop.bind(this);
+  }
+
+  componentWillMount() {
+
+    var estates = [
+      {
+        id: 'estate1',
+        name: "RaliCastle1"
+      }, {
+        id: 'estate2',
+        name: "RaliCastle2"
+      }, {
+        id: 'estate3',
+        name: "RaliCastle3"
+      }, {
+        id: 'estate4',
+        name: "RaliCastle3"
+      }
+    ];
+    this.handleEstates(estates);
+  }
+
+  handleEstates(estates) {
+    this.setState({estates});
+  }
+
+  renderItem(index, key) {
+    return <Element key={this.state.estates[index].id} className="element">{this.state.estates[index].name}
+    </Element>;
   }
 
   componentDidMount() {
-
-
-    Events.scrollEvent.register('begin', function() {
-      console.log("begin", arguments);
-    });
-
-    Events.scrollEvent.register('end', function() {
-      console.log("end", arguments);
-    });
-
     scrollSpy.update();
 
   }
@@ -36,30 +59,12 @@ class Main extends Component {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
   }
-  render () {
+  render() {
     return (
 
       <div>
 
-        <Element name="test1" className="element" >
-          Estate 1
-        </Element>
-
-        <Element name="test2" className="element">
-          Estate 2
-        </Element>
-
-        <Element name="test3" className="element">
-          Estate 3
-        </Element>
-
-        <Element name="test4" className="element">
-          Estate 4
-        </Element>
-
-        <Element name="test5" className="element">
-          Estate 5
-        </Element>
+        <ReactList itemRenderer={this.renderItem.bind(this)} length={this.state.estates.length} type='uniform'/>
 
         <a onClick={this.scrollToTop}>To the top!</a>
 
