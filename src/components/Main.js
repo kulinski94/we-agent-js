f09e20441969eb25341f739af2e670bb089dd94b
kulinski <tsvetelin_kulinski@abv.bg>
2017-05-28 17:30:02 +0300

    Changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,71 @@
 import React, { Component } from 'react';
 import './Main.css';
 
-class App extends Component {
-  render() {
+var Scroll    = require('react-scroll');
+
+var Element    = Scroll.Element;
+var Events     = Scroll.Events;
+var scroll     = Scroll.animateScroll;
+var scrollSpy  = Scroll.scrollSpy;
+
+class Main extends Component {
+
+  constructor (props){
+      super(props);
+      this.scrollToTop = this.scrollToTop.bind(this);
+  }
+
+  componentDidMount() {
+
+
+    Events.scrollEvent.register('begin', function() {
+      console.log("begin", arguments);
+    });
+
+    Events.scrollEvent.register('end', function() {
+      console.log("end", arguments);
+    });
+
+    scrollSpy.update();
+
+  }
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+  render () {
     return (
-      <div className="Main">
-        <p className="Main-estate">
-          Main part
-        </p>
+
+      <div>
+
+        <Element name="test1" className="element" >
+          Estate 1
+        </Element>
+
+        <Element name="test2" className="element">
+          Estate 2
+        </Element>
+
+        <Element name="test3" className="element">
+          Estate 3
+        </Element>
+
+        <Element name="test4" className="element">
+          Estate 4
+        </Element>
+
+        <Element name="test5" className="element">
+          Estate 5
+        </Element>
+
+        <a onClick={this.scrollToTop}>To the top!</a>
+
       </div>
     );
   }
-}
+};
 
-export default App;
+export default Main;
