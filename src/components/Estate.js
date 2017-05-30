24a7b046c459ce9e67e2a7910b648d497c83bca0
kulinski <tsvetelin_kulinski@abv.bg>
2017-05-30 08:02:17 +0300

    Simple grid

--- src/components/Estate.js
+++ src/components/Estate.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
-
+import Img from 'react-image'
 import './Estate.css';
 
 class Estate extends Component {
   render() {
     return (
       <div className="row">
-          <div className="col-md-12">
-            <p>{this.props.estate.name}</p>
-          </div>
-          <div className="col-lg-6">
-            <p className="Estate-description">{this.props.estate.description}</p>
-            <div className="col-lg-6">
-              <p>{this.props.estate.price}</p>
-            </div>
-          </div>
+        <div className="col-md-12">
+          <p>{this.props.estate.name}</p>
+        </div>
+        <div className="col-lg-6">
+          <p className="Estate-description">{this.props.estate.description}</p>
+        </div>
+        <div className="col-lg-6">
+          <p>{this.props.estate.price}$</p>
+        </div>
+        <div className="col-lg-6">
+          <Img className="Estate-image" src={this.props.estate.photos[0]}/>
+        </div>
       </div>
 
     );
