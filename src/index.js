d02265a91032d984daf400187e9665bf4c514bc0
kulinski <tsvetelin_kulinski@abv.bg>
2017-05-31 08:15:08 +0300

    Add react router dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import About from './components/About';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <BrowserRouter>
+    <div >
+      <Header/>
+      <Route exact path="/" component={Main}/>
+      <Route path="/about" component={About}/>
+      <Footer/>
+    </div>
+  </BrowserRouter>
+), document.getElementById('root'));
